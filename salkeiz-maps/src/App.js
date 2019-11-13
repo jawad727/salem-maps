8d060abbe91914e41582ae3da56a48117d07be6d
jawad727 <jawaddhussein@gmail.com>
2019-11-12 19:28:44 -0800

    map set up with single marker

---
+++ salkeiz-maps/src/App.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import './App.css';
+import ReactMapGL, {Marker} from "react-map-gl"
+
+
+const token = "pk.eyJ1IjoiamF3YWQ3MjciLCJhIjoiY2syd283cGY5MGhxZTNibzJsOGZvdzBsZCJ9.M4Ow-roauf6Sh7Yk-C44jA"
+const locations = [44.9429, -123.0351]
+
+function App() {
+  const [viewport, setViewport] = useState({
+    latitude: 44.9429,
+    longitude: -123.0351,
+    zoom: 13,
+    width: "100vw",
+    height: "100vh",
+    
+  })
+
+  return (
+    <div className="App">
+    <ReactMapGL {...viewport} mapboxApiAccessToken={token}
+    // mapStyle="mapbox://styles/jawad727/ck2wp8iui0eva1cmr5shukly8"
+    onViewportChange={(viewport) => {
+      setViewport(viewport)
+    }}>
+      <Marker latitude={locations[0]} longitude={locations[1]}>
+      <button>BUTTON</button>
+      </Marker>
+    </ReactMapGL>
+    </div>
+  );
+}
+
+export default App;

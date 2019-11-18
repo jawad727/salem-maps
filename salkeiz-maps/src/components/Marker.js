bc1a7fadf48a5299de66956b8bebab8421deef59
jawad727 <jawaddhussein@gmail.com>
2019-11-17 17:17:47 -0800

    names coming up on hover

--- salkeiz-maps/src/components/Marker.js
+++ salkeiz-maps/src/components/Marker.js
@@ -1,6 +1,7 @@
 
 import React, { Component } from "react";
 import {Marker} from "react-map-gl"
+import {TweenMax} from "gsap"
 
 class MarkerClass extends Component {
     state = {
@@ -13,14 +14,28 @@ class MarkerClass extends Component {
        console.log("componentEnd")
     }
 
+    editMenuAnimationOpen = () => { 
+        TweenMax.to(".markerButtons", 1, { transform: "translateX(0px)" })
+        // console.log("Worked")
+      }
+
+    
+
     render() { 
         {console.log(`aaa ${this.state.turnedoff}, ${this.props.item.number} -> ${this.props.authnumber}`)}
       return (
 
-            <Marker latitude={this.props.item.latitude} longitude={this.props.item.longitude} key={this.props.item.number}>
+            <Marker className="aaa" latitude={this.props.item.latitude} longitude={this.props.item.longitude} key={this.props.item.number}>
                 <button className="markerButtons" onClick={() => (this.setState({turnedoff: !this.state.turnedoff}) )} style={this.state.turnedoff ? {backgroundColor: "darkred"} : null} ></button>
-                
+                <div className="describerContainer">
+                    <div className="arrowUp" />
+                    <div className="describer">
+                        {this.props.item.name}
+                    </div>
+                    
+                </div>
             </Marker> 
+            
         
       );
     }
